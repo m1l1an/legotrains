3ae095c692aec1c5398b1fb24339c03cbd0aac20
Richard G. Roman <rits@rits.hu>
2025-11-06 11:33:00 +0100

    Moved experiments to separate dir

---
+++ experiments/textual-test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# (C) 2025, Richard G. Roman <rits@rits.hu>
+# Created on 2025-11-05 21:13:11
+""" """
+
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer, Label, DataTable
+
+class EmojiDemo(App):
+    def compose(self) -> ComposeResult:
+        yield Header(show_clock=True)
+        table = DataTable()
+        table.add_columns("🐋 Pod", "💚 Status", "⚙️ CPU", "💾 Memory")
+        table.add_rows([
+            ("nginx-1", "✅ Running", "12m", "128Mi"),
+            ("redis-2", "🕒 Pending", "0m", "0Mi"),
+        ])
+        yield table
+        yield Label("Press Q to quit 😎")
+        yield Footer()
+
+if __name__ == "__main__":
+    EmojiDemo().run()
+
